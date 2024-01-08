629e2b882041441a0bc3ba2b9c9af1a360b7989c
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2024-01-08 15:21:36 +0100

    Schedule added so folders can be synchronized regulary, logging adjusted

--- sync_folders.py
+++ sync_folders.py
@@ -6,22 +6,9 @@ import os
 import hashlib
 import shutil
 import logging
-
-logger = logging.getLogger("sync_folders_logger")
-logger.setLevel(logging.DEBUG)
-
-file_handler = logging.FileHandler("log.log")
-file_handler.setLevel(logging.DEBUG)
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter("%(levelname)s - %(message)s")
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+import argparse
+import schedule
+import time
 
 
 def calculate_md5(path):
@@ -41,16 +28,13 @@ def calculate_md5(path):
     return md5.hexdigest()
 
 
-source = r"C:\Users\marek\OneDrive\Plocha\Test\source"
-replica = r"C:\Users\marek\OneDrive\Plocha\Test\replica"
-
-
-print(f"SRC: {calculate_md5(source)} - REPLICA: {calculate_md5(replica)}")
-
-
-# Loop through the files in source folder
-# How to loop through both files and compare the files with the same name?
-def sync_folders(source, replica):
+def sync_folders(
+    source,
+    replica,
+    log,
+    logger,
+    initial=True,
+):
     with os.scandir(source) as items:
         for item in items:
             file_name = item.name
@@ -58,62 +42,111 @@ def sync_folders(source, replica):
             source_path = os.path.join(source, file_name)
             replica_path = os.path.join(replica, file_name)
 
-            # checks if the file/folder exist in the replica folder
             if os.path.exists(replica_path):
-                # checks if the files/folders are not identical
                 if calculate_md5(replica_path) != calculate_md5(source_path):
-                    # copy file to replica folder
                     if os.path.isfile(source_path):
                         shutil.copy2(source_path, replica)
                         logger.info(f"File '{file_name}' COPIED to {replica}")
-                    # copy folder to replica folder
+
                     elif os.path.isdir(source_path):
                         shutil.rmtree(replica_path)
                         shutil.copytree(source_path, replica_path)
                         logger.info(f"Folder '{file_name}' COPIED to {replica}")
 
-            # if file/folder does not exist in replica folder
             else:
-                # copy file to replica folder
                 if os.path.isfile(source_path):
                     shutil.copy2(source_path, replica)
-                    # LOGGING - file CREATED at SOURCE
-                    logger.info(f"File '{file_name}' CREATED at {source}")
-                    # LOGGING - file COPIED to REPLICA
+                    if not initial:
+                        logger.info(f"File '{file_name}' CREATED at {source}")
                     logger.info(f"File '{file_name}' COPIED to {replica}")
-                # copy folder to replica folder
+
                 elif os.path.isdir(source_path):
                     shutil.copytree(source_path, replica_path)
-                    logger.info(f"Folder '{file_name}' CREATED at {source}")
-                    # LOGGING - folder COPIED to REPLICA
+                    if not initial:
+                        logger.info(f"Folder '{file_name}' CREATED at {source}")
                     logger.info(f"Folder '{file_name}' COPIED to {replica}")
 
-    # Loop through the files in replica folder
     with os.scandir(replica) as items:
         for item in items:
             file_name = item.name
             source_path = os.path.join(source, file_name)
             replica_path = os.path.join(replica, file_name)
 
-            # remove any redundant files/folders in the replica folder
-            # log this operation into the logfile and to the console
             if not os.path.exists(source_path):
                 if os.path.isfile(replica_path):
                     os.remove(replica_path)
-                    # LOGGING - file REMOVED at SOURCE
                     logger.info(f"File '{file_name}' REMOVED at {source}")
-                    # LOGGING - file REMOVED at REPLICA
                     logger.info(f"File '{file_name}' REMOVED at {replica}")
+
                 elif os.path.isdir(replica_path):
                     shutil.rmtree(replica_path)
-                    # LOGGING - folder REMOVED at SOURCE
                     logger.info(f"Folder '{file_name}' REMOVED at {source}")
-                    # LOGGING - folder REMOVED at REPLICA
                     logger.info(f"Folder '{file_name}' REMOVED at {replica}")
 
-    print(f"SRC: {calculate_md5(source)} - REPLICA: {calculate_md5(replica)}")
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A synchronization tool that ensures the replica folder mirrors the contents of the source folder, maintaining a complete and identical copy of the source directory."
+    )
+    parser.add_argument(
+        "source",
+        metavar="source",
+        type=str,
+        help="Enter the path to a source folder you want to synchronize",
+    )
+    parser.add_argument(
+        "replica",
+        metavar="replica",
+        type=str,
+        help="Enter the path to a replica folder for synchrozization",
+    )
+
+    parser.add_argument(
+        "log",
+        metavar="log",
+        type=str,
+        help="Enter the path where to store the log file. Make sure it is not the path to source or replica folder you want to synchronize",
+    )
+
+    args = parser.parse_args()
+    source = rf"{args.source}"
+    replica = rf"{args.replica}"
+    log = rf"{args.log}"
+
+    logger = logging.getLogger("sync_folders_logger")
+    logger.setLevel(logging.DEBUG)
+
+    log_path = rf"{log}\log.log"
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    if log == source or log == source:
+        raise ValueError(
+            "The log path must be different from the source and replica paths."
+        )
+    # print(f"SRC: {calculate_md5(source)} - REPLICA: {calculate_md5(replica)}")
+    sync_folders(source, replica, log, logger)
+    schedule.every(15).seconds.do(
+        lambda: sync_folders(source, replica, log, logger, initial=False)
+    )
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        # print(f"SRC: {calculate_md5(source)} - REPLICA: {calculate_md5(replica)}")
+
+    # schedule periodic synchronization
 
 
-sync_folders(source, replica)
-
-# schedule periodic synchronization
+if __name__ == "__main__":
+    main()
